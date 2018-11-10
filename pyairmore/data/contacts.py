197c284d4b287f3e2e10cb1ed596de97db2a5575
Eray Erdin <eraygezer.94@gmail.com>
2018-11-10 21:35:04 +0300

    added Group

--- pyairmore/data/contacts.py
+++ pyairmore/data/contacts.py
@@ -19,3 +19,27 @@ class Source:
 
     def __ne__(self, other: "Source") -> bool:
         return not self.__eq__(other)
+
+
+class Group:
+    # todo 1 - class doc
+
+    def __init__(self):
+        self.id = None  # type: str
+        self.name = None  # type: str
+
+    def __eq__(self, other: "Group") -> bool:
+        if self is other:
+            return True
+
+        val = all((
+            self.id == other.id,
+        ))
+
+        # change name
+        self.name = other.name
+
+        return val
+
+    def __ne__(self, other: "Group") -> bool:
+        return not self.__eq__(other)
--- tests/test_data/test_contacts.py
+++ tests/test_data/test_contacts.py
@@ -25,3 +25,37 @@ class SourceComparisonTestCase(unittest.TestCase):
     def test_not_equal(self):
         self.assertNotEqual(self.s1, self.s3)
         self.assertNotEqual(self.s2, self.s3)
+
+
+class GroupComparisonTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.g1 = pyairmore.data.contacts.Group()
+        cls.g1.id = "1"
+        cls.g1.name = "Foo"
+
+        cls.g2 = pyairmore.data.contacts.Group()
+        cls.g2.id = "1"
+        cls.g2.name = "Foo"
+
+        cls.g3 = pyairmore.data.contacts.Group()
+        cls.g3.id = "1"
+        cls.g3.name = "Bar"
+
+        cls.g4 = pyairmore.data.contacts.Group()
+        cls.g4.id = "2"
+        cls.g4.name = "Foo"
+
+    def test_equal(self):
+        self.assertEqual(self.g1, self.g2)
+        self.assertEqual(self.g1, self.g3)
+
+    def test_not_equal(self):
+        self.assertNotEqual(self.g1, self.g4)
+        self.assertNotEqual(self.g2, self.g4)
+        self.assertNotEqual(self.g3, self.g4)
+
+    def test_equal_name_reassingment(self):
+        self.assertEqual(self.g2, self.g3)
+        self.assertEqual(self.g2.name, "Bar")
