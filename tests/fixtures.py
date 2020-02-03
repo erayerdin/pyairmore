df42f16335215aa076d77781ec63bad291098d19
Eray Erdin <eraygezer.94@gmail.com>
2020-02-03 22:14:29 +0300

    added test_is_application_open_zero

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_httpserver import HTTPServer
 
 import pyairmore.request
 
@@ -8,3 +9,10 @@ def airmore_session():
     from ipaddress import IPv4Address
 
     return pyairmore.request.AirmoreSession(IPv4Address("127.0.0.1"))
+
+
+@pytest.fixture
+def authorization_required(httpserver: HTTPServer):
+    httpserver.expect_request(
+        "/", "POST", query_string={"Key": "PhoneRequestAuthorization"}
+    ).respond_with_data("true")
--- tests/test_session.py
+++ tests/test_session.py
@@ -1,2 +1,14 @@
+from pytest_httpserver import HTTPServer
+
+
 def test_is_server_running(httpserver, airmore_session):
     assert airmore_session.is_server_running
+
+
+def test_is_application_open_zero(
+    httpserver: HTTPServer, authorization_required, airmore_session
+):
+    httpserver.expect_request(
+        "/", "POST", query_string={"Key": "PhoneCheckAuthorization"}
+    ).respond_with_data('"0"')
+    assert airmore_session.is_application_open
