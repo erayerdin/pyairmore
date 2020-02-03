e2ee573f70a70e5545c4acaaf0717c3ca8e8cd9e
Eray Erdin <eraygezer.94@gmail.com>
2020-02-03 12:45:46 +0300

    updated TestSendMessageRequest to use fixtures

--- tests/test_request/test_messaging.py
+++ tests/test_request/test_messaging.py
@@ -5,11 +5,11 @@ import unittest
 import pytest
 
 import pyairmore.request
-from pyairmore.request.messaging import MessageHistoryRequest
+from pyairmore.request.messaging import MessageHistoryRequest, SendMessageRequest
 
 
 @pytest.fixture
-def _message_history_request(airmore_request_factory, airmore_session):
+def _message_history_request(airmore_request_factory):
     return airmore_request_factory(MessageHistoryRequest)
 
 
@@ -24,39 +24,40 @@ class TestMessageHistoryRequest:
         assert _message_history_request.method == "POST"
 
 
-class SendMessageRequestTestCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.session = pyairmore.request.AirmoreSession(
-            ipaddress.IPv4Address("127.0.0.1")
-        )
-        cls.request = pyairmore.request.messaging.SendMessageRequest(
-            cls.session, "321", "foo"
-        )
-        if isinstance(cls.request.body, bytes):
-            cls.body = json.loads(cls.request.body.decode("utf-8"))[0]  # type: dict
-        else:
-            cls.body = json.loads(cls.request.body)[0]  # type: dict
+@pytest.fixture
+def _send_message_request(airmore_request_factory):
+    return airmore_request_factory(SendMessageRequest, "321", "foo")
 
-    def test_url_startswith(self):
-        self.assertTrue(self.request.url.startswith(self.session.base_url))
 
-    def test_url_endswith(self):
-        self.assertTrue(self.request.url.endswith("/?Key=MessageSend"))
+@pytest.fixture
+def _send_message_request_body(_send_message_request):
+    if isinstance(_send_message_request.body, bytes):
+        body = json.loads(_send_message_request.body.decode("utf-8"))[0]  # type: dict
+    else:
+        body = json.loads(_send_message_request.body)[0]  # type: dict
 
-    def test_method(self):
-        self.assertEqual(self.request.method, "POST")
+    return body
 
-    def test_body_phone(self):
-        self.assertEqual(self.body.get("Phone"), "321")
 
-    def test_body_content(self):
-        self.assertEqual(self.body.get("Content"), "foo")
+class TestSendMessageRequest:
+    def test_url_startswith(self, _send_message_request, airmore_session):
+        assert _send_message_request.url.startswith(airmore_session.base_url)
 
-    def test_body_uid(self):
-        self.assertIsInstance(self.body.get("UniqueID"), str)
-        self.assertGreater(len(self.body.get("UniqueID")), 0)
+    def test_url_endswith(self, _send_message_request):
+        assert _send_message_request.url.endswith("/?Key=MessageSend")
+
+    def test_method(self, _send_message_request):
+        assert _send_message_request.method == "POST"
+
+    def test_body_phone(self, _send_message_request_body):
+        assert _send_message_request_body.get("Phone") == "321"
+
+    def test_body_content(self, _send_message_request_body):
+        assert _send_message_request_body.get("Content") == "foo"
+
+    def test_body_uid(self, _send_message_request_body):
+        assert isinstance(_send_message_request_body.get("UniqueID"), str)
+        assert len(_send_message_request_body.get("UniqueID")) > 0
 
 
 class ChatHistoryRequestTestCase(unittest.TestCase):
